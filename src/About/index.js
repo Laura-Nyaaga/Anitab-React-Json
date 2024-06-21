2ff86a86caa35335039cdd4de17ebdf8dac8ed49
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-06-21 12:56:49 +0300

    fetched user profiles and fullnames from JsonDummy

---
+++ src/About/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+const About = ()=>{
+    return(
+      <div className="about-text">
+        <p>
+          We are learning JSX</p>
+         <p>We are coding a new front-end</p>
+      </div>
+    )
+};
+export default About;
